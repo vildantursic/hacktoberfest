ed335a7fcb647f758afeaa42417ae481bebb83c7
Amar Bešlija <amarbeslija13@gmail.com>
2017-10-14 21:00:02 +0200

    Added a comment
    Added a comment for copyOnClick function

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/* This function takes a git command, makes an fake input field
+ * and then copy it to the clipboard for use.
+ */
 function copyOnClick(el) {
   let input = document.createElement("input");
   document.body.append(input);
